df42ff2691b2b8ab2b729cd425c5b94b4d76cf20
LEVONO <kcdbordios.work@gmail.com>
2025-06-15 19:00:17 +0800

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+void main() => runApp(const NetCafeAd());
+
+class NetCafeAd extends StatefulWidget {
+  const NetCafeAd({super.key});
+  @override
+  State<NetCafeAd> createState() => _NetCafeAdState();
+}
+class _NetCafeAdState extends State<NetCafeAd> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool('isDark') ?? false;
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  Future<void> _toggleTheme(bool isDark) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDark', isDark);
+    setState(() {
+      _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.grey,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.black87,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.grey[900],
+        primaryColor: Colors.amber,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black87,
+          foregroundColor: Colors.amber,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(fontWeight: FontWeight.bold, color: Colors.white70),
+        ),
+      ),
+      themeMode: _themeMode,
+      //home: TodoHome(themeMode: _themeMode, onThemeChanged: _toggleTheme),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
