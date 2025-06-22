17ac90cc7beb00dedd60fed551685cee0b75e141
LEVONO <kcdbordios.work@gmail.com>
2025-06-22 12:33:53 +0800

    Added new file for reusable widgets

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ToggleMode extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+    );
   }
 }
