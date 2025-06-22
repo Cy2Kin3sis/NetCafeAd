5f320e1c0e8af4a8549a4d45a61286683cd5d15d
LEVONO <kcdbordios.work@gmail.com>
2025-06-22 12:53:03 +0800

    Reusable AppBar fix

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ToggleMode extends StatelessWidget {
-
+class ToggleBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool isDark;
+  final ThemeMode themeMode;
+  final Function(bool) onThemeChanged;
+  const ToggleBar({super.key, required this.title, required this.isDark, required this.themeMode, required this.onThemeChanged});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return AppBar(
+      title: Text(title),
+      automaticallyImplyLeading: false,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(isDark ? Icons.dark_mode : Icons.light_mode),
+              Switch(
+                value: isDark,
+                onChanged: onThemeChanged,
+                activeColor: Colors.amber,
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
