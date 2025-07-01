62e2fc3c844e1d88c810f8455923c18d45ce14f1
LEVONO <kcdbordios.work@gmail.com>
2025-07-01 17:08:23 +0800

    Made arrows vanishing for it to be less annoying in web/desktop

--- lib/home.dart
+++ lib/home.dart
@@ -14,6 +14,7 @@ class BDGiT extends StatefulWidget {
 class BDGiTState extends State<BDGiT> {
   final PageController _pageController = PageController();
   int _currentPage = 0;
+  bool _isHovering = false;
 
   // Get the list of pages
   List<Widget> get _pages => [
@@ -52,72 +53,86 @@ class BDGiTState extends State<BDGiT> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageView.builder(
-            controller: _pageController,
-            itemCount: _pages.length,
-            onPageChanged: (index) => setState(() => _currentPage = index),
-            itemBuilder: (context, index) => _pages[index],
-          ),
-          // Navigation indicators
-          Positioned(
-            bottom: 30,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                _pages.length,
-                    (index) => Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 4),
-                  width: 8,
-                  height: 8,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentPage == index
-                        ? Colors.amber
-                        : Colors.amber.withAlpha(77),
+  Widget build(BuildContext context) => Scaffold(
+      body: MouseRegion(
+        onEnter: (_) => setState(() => _isHovering = true),
+        onExit: (_) => setState(() => _isHovering = false),
+        child: Stack(
+          children: [
+            PageView.builder(
+              controller: _pageController,
+              itemCount: _pages.length,
+              onPageChanged: (index) => setState(() => _currentPage = index),
+              itemBuilder: (context, index) => _pages[index],
+            ),
+
+            // Navigation indicators
+            Positioned(
+              bottom: 30,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  _pages.length,
+                      (index) => Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentPage == index
+                          ? Colors.amber
+                          : Colors.amber.withAlpha(77),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          // Navigation arrows
-          if (_currentPage > 0)
-            Positioned(
-              left: 10,
-              top: MediaQuery.of(context).size.height / 2 - 30,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black.withValues(alpha: 0.3),
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back, color: Colors.amber),
-                  onPressed: _previousPage,
-                ),
-              ),
-            ),
-          if (_currentPage < _pages.length - 1)
-            Positioned(
-              right: 10,
-              top: MediaQuery.of(context).size.height / 2 - 30,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black.withValues(alpha: 0.3),
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_forward, color: Colors.amber),
-                  onPressed: _nextPage,
+
+            // Left arrow (previous)
+            if (_currentPage > 0)
+              Positioned(
+                left: 10,
+                top: MediaQuery.of(context).size.height / 2 - 30,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 200),
+                  opacity: _isHovering ? 1.0 : 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black.withAlpha(75),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_back, color: Colors.amber),
+                      onPressed: _previousPage,
+                    ),
+                  ),
                 ),
               ),
-            ),
-        ],
+
+            // Right arrow (next)
+            if (_currentPage < _pages.length - 1)
+              Positioned(
+                right: 10,
+                top: MediaQuery.of(context).size.height / 2 - 30,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 200),
+                  opacity: _isHovering ? 1.0 : 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black.withAlpha(75),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_forward, color: Colors.amber),
+                      onPressed: _nextPage,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
-  }
 }
