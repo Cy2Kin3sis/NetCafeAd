2fb96f48ab3accd27ce1682d6a873db8eb03f8fc
LEVONO <kcdbordios.work@gmail.com>
2025-07-05 08:02:00 +0800

    Image slideshow

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'constants.dart';
@@ -85,6 +87,252 @@ class BlogPost extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class ImageSlideshow extends StatefulWidget {
+  final List<String> images;
+  final double height;
+  final Duration autoPlayDuration;
+  final bool autoPlay;
+  final bool showDots;
+  final bool showArrows;
+  final BoxFit fit;
+  final BorderRadius? borderRadius;
+
+  const ImageSlideshow({
+    super.key,
+    required this.images,
+    this.height = 300,
+    this.autoPlayDuration = const Duration(seconds: 3),
+    this.autoPlay = true,
+    this.showDots = true,
+    this.showArrows = true,
+    this.fit = BoxFit.cover,
+    this.borderRadius,
+  });
+
+  @override
+  State<ImageSlideshow> createState() => _ImageSlideshowState();
+}
+
+class _ImageSlideshowState extends State<ImageSlideshow> {
+  late PageController _pageController;
+  int _currentIndex = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    if (widget.autoPlay) {
+      _startAutoPlay();
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _startAutoPlay() {
+    _timer = Timer.periodic(widget.autoPlayDuration, (timer) {
+      if (_currentIndex < widget.images.length - 1) {
+        _nextImage();
+      } else {
+        _goToImage(0);
+      }
+    });
+  }
+
+  void _stopAutoPlay() {
+    _timer?.cancel();
+  }
+
+  void _nextImage() {
+    if (_currentIndex < widget.images.length - 1) {
+      _goToImage(_currentIndex + 1);
+    }
+  }
+
+  void _previousImage() {
+    if (_currentIndex > 0) {
+      _goToImage(_currentIndex - 1);
+    }
+  }
+
+  void _goToImage(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.images.isEmpty) {
+      return Container(
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: widget.borderRadius,
+        ),
+        child: const Center(
+          child: Icon(
+            Icons.image_not_supported,
+            size: 50,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    }
+
+    return SizedBox(
+      height: widget.height,
+      child: Stack(
+        children: [
+          // Image PageView
+          ClipRRect(
+            borderRadius: widget.borderRadius ?? BorderRadius.zero,
+            child: PageView.builder(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              itemCount: widget.images.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    if (widget.autoPlay) {
+                      _stopAutoPlay();
+                      Future.delayed(const Duration(seconds: 5), () {
+                        _startAutoPlay();
+                      });
+                    }
+                  },
+                  child: Image.network(
+                    widget.images[index],
+                    fit: widget.fit,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        color: Colors.grey[300],
+                        child: const Center(
+                          child: Icon(
+                            Icons.broken_image,
+                            size: 50,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      );
+                    },
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Container(
+                        color: Colors.grey[200],
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                                : null,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // Navigation Arrows
+          if (widget.showArrows && widget.images.length > 1) ...[
+            // Left Arrow
+            Positioned(
+              left: 10,
+              top: 0,
+              bottom: 0,
+              child: Center(
+                child: GestureDetector(
+                  onTap: _previousImage,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withAlpha(128),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // Right Arrow
+            Positioned(
+              right: 10,
+              top: 0,
+              bottom: 0,
+              child: Center(
+                child: GestureDetector(
+                  onTap: _nextImage,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withAlpha(128),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+
+          // Dots Indicator
+          if (widget.showDots && widget.images.length > 1)
+            Positioned(
+              bottom: 10,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  widget.images.length,
+                      (index) => GestureDetector(
+                    onTap: () => _goToImage(index),
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 4),
+                      width: 8,
+                      height: 8,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _currentIndex == index
+                            ? Colors.white
+                            : Colors.white.withAlpha(128),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
