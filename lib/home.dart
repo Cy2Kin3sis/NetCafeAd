2e5bbc5aa522baa35a6f395d48b865f86967475a
LEVONO <kcdbordios.work@gmail.com>
2025-07-27 13:28:53 +0800

    Bottom navigation

--- lib/home.dart
+++ lib/home.dart
@@ -13,39 +13,46 @@ class BDGiT extends StatefulWidget {
   BDGiTState createState() => BDGiTState();
 }
 class BDGiTState extends State<BDGiT> {
+  int _currentIndex = 0;
   final PageController _pageController = PageController();
-  int _currentPage = 0;
-  bool _isHovering = false;
+
+  // Define titles for each tab
 
   // Get the list of pages
   List<Widget> get _pages => [
-    CafeInfoScreen(themeMode: widget.themeMode, isDark: widget.themeMode == ThemeMode.dark, onThemeChanged: widget.onThemeChanged),
-    MeetTheTeamScreen(themeMode: widget.themeMode, isDark: widget.themeMode == ThemeMode.dark, onThemeChanged: widget.onThemeChanged, executives: executives),
-    BusinessLoreScreen(themeMode: widget.themeMode, isDark: widget.themeMode == ThemeMode.dark, onThemeChanged: widget.onThemeChanged, timeline: timeline),
-    CompanyBlogScreen(themeMode: widget.themeMode, isDark: widget.themeMode == ThemeMode.dark, onThemeChanged: widget.onThemeChanged),
+    CafeInfoScreen(
+        themeMode: widget.themeMode,
+        isDark: widget.themeMode == ThemeMode.dark,
+        onThemeChanged: widget.onThemeChanged
+    ),
+    MeetTheTeamScreen(
+        themeMode: widget.themeMode,
+        isDark: widget.themeMode == ThemeMode.dark,
+        onThemeChanged: widget.onThemeChanged,
+        executives: executives
+    ),
+    BusinessLoreScreen(
+        themeMode: widget.themeMode,
+        isDark: widget.themeMode == ThemeMode.dark,
+        onThemeChanged: widget.onThemeChanged,
+        timeline: timeline
+    ),
+    CompanyBlogScreen(
+        themeMode: widget.themeMode,
+        isDark: widget.themeMode == ThemeMode.dark,
+        onThemeChanged: widget.onThemeChanged
+    ),
   ];
 
-
-  void _nextPage() {
-    if (_currentPage < _pages.length - 1) {
-      setState(() => _currentPage++);
-      _pageController.animateToPage(
-        _currentPage,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
-  }
-
-  void _previousPage() {
-    if (_currentPage > 0) {
-      setState(() => _currentPage--);
-      _pageController.animateToPage(
-        _currentPage,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
   }
 
   @override
@@ -55,86 +62,38 @@ class BDGiTState extends State<BDGiT> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      body: MouseRegion(
-        onEnter: (_) => setState(() => _isHovering = true),
-        onExit: (_) => setState(() => _isHovering = false),
-        child: Stack(
-          children: [
-            PageView.builder(
-              controller: _pageController,
-              itemCount: _pages.length,
-              onPageChanged: (index) => setState(() => _currentPage = index),
-              itemBuilder: (context, index) => _pages[index],
-            ),
-
-            // Navigation indicators
-            Positioned(
-              bottom: 30,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  _pages.length,
-                      (index) => Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 4),
-                    width: 8,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: _currentPage == index
-                          ? themeColor
-                          : themeColor.withAlpha(77),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            // Left arrow (previous)
-            if (_currentPage > 0)
-              Positioned(
-                left: 10,
-                top: MediaQuery.of(context).size.height / 2 - 30,
-                child: AnimatedOpacity(
-                  duration: const Duration(milliseconds: 200),
-                  opacity: _isHovering ? 1.0 : 0.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black.withAlpha(75),
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      icon: const Icon(Icons.arrow_back, color: themeColor),
-                      onPressed: _previousPage,
-                    ),
-                  ),
-                ),
-              ),
-
-            // Right arrow (next)
-            if (_currentPage < _pages.length - 1)
-              Positioned(
-                right: 10,
-                top: MediaQuery.of(context).size.height / 2 - 30,
-                child: AnimatedOpacity(
-                  duration: const Duration(milliseconds: 200),
-                  opacity: _isHovering ? 1.0 : 0.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black.withAlpha(75),
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      icon: const Icon(Icons.arrow_forward, color: themeColor),
-                      onPressed: _nextPage,
-                    ),
-                  ),
-                ),
-              ),
-          ],
-        ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) => setState(() => _currentIndex = index),
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        selectedItemColor: themeColor,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Info',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Team',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Lore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: 'Blog',
+          ),
+        ],
       ),
     );
+  }
 }
